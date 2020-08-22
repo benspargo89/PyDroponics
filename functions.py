6732a93c73d57f345c2a3e45d3987d44edff520c
Ben Spargo <benspargo89@gmail.com>
2020-08-22 10:53:33 -0500

    added pump class

--- functions.py
+++ functions.py
@@ -1,10 +1,54 @@
+from gpiozero import LED
+
+
+        
+class pump_control:
+    def __init__(self, pin, start_state=None):
+        self.switch = LED(pin)
+        if start_state:
+            if start_state == 'on':
+                self.pump_on()
+            else:
+                self.pump_off()
+        else:
+            self.init_state()
+        
+    def read_state(self):
+        with open('pump_state.txt') as file:
+            data = file.read().strip()
+        return data
+    
+    def write_state(self, state):
+        with open('pump_state.txt', 'w+') as file:
+            file.write(state)
+    
+    def pump_on(self):
+        self.switch.on()
+        self.write_state('on')
+    
+    def pump_off(self):
+        self.switch.off()
+        self.write_state('off')
+
+    
+    def init_state(self):
+        ls = self.read_state()
+        if ls == 'on':
+            self.pump_on()
+        else:
+            self.switch.off()
+            
+ 
+pump = pump_control(4)
+
+
 def read_sensor_data():
-	port = "COM3"
-	baudrate = 9600
-	with Serial(port=port, baudrate=baudrate, timeout=1) as Port:
-		Port.flushInput()    
-		line = Port.readline().decode().strip()
-		while line == "":
-			line = Port.readline().decode().strip()
-		Port.close()
-	return {item.split(':')[0]:item.split(':')[1] for item in line.split()}
+    port = "COM3"
+    baudrate = 9600
+    with Serial(port=port, baudrate=baudrate, timeout=1) as Port:
+        Port.flushInput()    
+        line = Port.readline().decode().strip()
+        while line == "":
+            line = Port.readline().decode().strip()
+        Port.close()
+    return {item.split(':')[0]:item.split(':')[1] for item in line.split()}
--- views.py
+++ views.py
@@ -7,16 +7,6 @@ from serial import Serial
 
 app = Flask(__name__)
 
-def read_sensor_data():
-	port = "COM3"
-	baudrate = 9600
-	with Serial(port=port, baudrate=baudrate, timeout=1) as Port:
-		Port.flushInput()    
-		line = Port.readline().decode().strip()
-		while line == "":
-			line = Port.readline().decode().strip()
-		Port.close()
-	return {item.split(':')[0]:item.split(':')[1] for item in line.split()}
 
 @app.route("/")
 def index():
@@ -24,5 +14,5 @@ def index():
 
 @app.route("/sensor_data")
 def sensor_data():
-	sensor_data = read_sensor_data()
-	return jsonify(temperature=sensor_data['Temperature'], humidity=sensor_data['Humidity'])
+    sensor_data = read_sensor_data()
+    return jsonify(temperature=sensor_data['Temperature'], humidity=sensor_data['Humidity'])
